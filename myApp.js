0c3fa16ce232ad0c1e14d2297b5bc2373f7f4d13
Terng Sahasbhop <sahasbhop.suvadhanabhakdi@scbtechx.io>
2022-03-30 18:50:18 +0700

    Serve an HTML File

--- myApp.js
+++ myApp.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get(`/`, (req, res) => {
- res.send(`Hello Express`)
+    res.sendFile(`${__dirname}/views/index.html`)
 })
 
 module.exports = app;
