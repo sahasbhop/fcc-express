76f300c1be9ef0c73ffd725bfd5602a979e8b290
Terng Sahasbhop <sahasbhop.suvadhanabhakdi@scbtechx.io>
2022-04-01 14:26:09 +0700

    Serve JSON on a Specific Route

--- myApp.js
+++ myApp.js
@@ -6,5 +6,8 @@ app.use('/public', express.static(path.join(__dirname, 'public')))
 app.get(`/`, (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'index.html'))
 })
+app.get('/json', (req, res) => {
+    res.json({message: "Hello json"})
+})
 
 module.exports = app;
