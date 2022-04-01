5bb6f7298b69d71572ad4e43ea6396de5306f39e
Terng Sahasbhop <sahasbhop.suvadhanabhakdi@scbtechx.io>
2022-04-01 14:13:44 +0700

    Serve Static Assets

--- myApp.js
+++ myApp.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const path = require('path')
 const app = express();
 
+app.use('/public', express.static(path.join(__dirname, 'public')))
 app.get(`/`, (req, res) => {
-    res.sendFile(`${__dirname}/views/index.html`)
+    res.sendFile(path.join(__dirname, 'views', 'index.html'))
 })
 
 module.exports = app;
